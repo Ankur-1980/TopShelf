9551c0b8c2a171d25d3afeaefabc768829e572a5
Ankur <vasavada.ankur@gmail.com>
2020-06-24 16:47:10 -0400

    changed other server port to 80

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 
 const recipes = require("./routes/recipes");
 const users = require("./routes/users");
